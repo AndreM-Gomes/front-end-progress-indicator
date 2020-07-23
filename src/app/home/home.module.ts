affd9b42c481053b053d1e946f50e7eaa9a27bdc
Andre Gomes <andrem.gomes@outlook.com>
2020-07-23 16:38:06 -0300

    Break main task list in components

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,12 +1,14 @@
+import { TaskCardComponent } from './main-task-list/task-card/task-card.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { MainTaskListComponent } from './main-task-list/main-task-list.component';
 
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, MainTaskListComponent, TaskCardComponent],
   imports: [
     CommonModule,
     HomeRoutingModule
